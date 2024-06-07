6378546623407765a6fc79bb1668d108ec234cd3
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-06-07 17:32:07 -0300

    fix: config add sass

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    include: ['src/**/*.spec.ts'],
     coverage: {
       reporter: ['text', 'json', 'html'],
     }
